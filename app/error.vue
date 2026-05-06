0cbf80be9cbdac763e8afa2fd6c8314a32be436e
Yohannes Alemu <johannesalemu01@gmail.com>
2026-05-06 20:19:52 +0300

    initial commit

---
+++ app/error.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+// const props = defineProps<{ error: NuxtError }>();
+</script>
+
+<template>
+  <div>
+    <!-- <h1>{{ error.status }}</h1> -->
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
